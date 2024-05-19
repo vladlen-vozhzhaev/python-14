38f2f00b6d97a9c1f2ef8642024db878e3166ed4
Vlalden <vladlen@vozhzhaev.ru>
2024-05-19 13:37:02 +0300

    add userName

--- client.py
+++ client.py
@@ -1,9 +1,18 @@
 # Клиент
 import socket
+import threading
 clientSocket = socket.socket()
 clientSocket.connect(('127.0.0.1', 9123))
-while True:
-    message = input("Введите сообщение: ")
-    clientSocket.send(message.encode())
-    data = clientSocket.recv(1024)
-    print(f"Ответ от сервера: {data.decode()}")
+def sendMessage():
+    while True:
+        message = input("Введите сообщение: \n")
+        clientSocket.send(message.encode())
+def getReponse():
+    while True:
+        data = clientSocket.recv(1024)
+        print(data.decode())
+        print("Введите сообщение: ")
+
+thread1 = threading.Thread(target=sendMessage)
+thread1.start()
+getReponse()
--- server.py
+++ server.py
@@ -5,15 +5,21 @@ serverSocket = socket.socket()
 serverSocket.bind(('', 9123))
 serverSocket.listen(1)
 print("Сервер запущен")
-def chatting():
+sockets = []
+def chatting(clientSocket):
+    clientSocket.send("Введите имя: ".encode())
+    userName = clientSocket.recv(1024)
+    userName = userName.decode()
     while True:
         data = clientSocket.recv(1024)
-        print("Сообщение от клиента: "+data.decode())
-        clientSocket.send(data.decode().upper().encode())
+        print(f"{userName}: {data.decode()}")
+        for socket in sockets:
+            socket.send(f"{userName}: {data.decode()}".encode())
 
 while True:
     print("Ожидаем подключения клиента...")
     clientSocket, addr = serverSocket.accept()
     print(f"Клиент подключился {addr}")
-    thread = threading.Thread(target=chatting)
+    sockets.append(clientSocket)
+    thread = threading.Thread(target=chatting, args=(clientSocket,))
     thread.start()
